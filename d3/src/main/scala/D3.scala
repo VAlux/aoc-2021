44ab7d50e3c916e54fa795bcf1621f2edfb2e3c1
voievolk <oleksandr.voievodin@externals.adidas.com>
2021-12-17 09:05:39 +0200

    d3 part 2 WIP

--- d3/src/main/scala/D3.scala
+++ d3/src/main/scala/D3.scala
@@ -1,14 +1,17 @@
 import scala.util.chaining.*
+import scala.annotation.tailrec
 
 case class DiagnosticData(gamma: String, epsilon: String)
 
 @main def entrypoint: Unit =
-  val input = FileLoader.readFile("input.txt")
+  val input = FileLoader.readFile("input_test.txt")
   val resultPart1 = run(input)
   println(resultPart1)
 
 def run(input: List[String]): Int =
-  transpose(input).map(calcDiagnosticData).pipe(reduceDiagnosticData).pipe(calcDiagnosticResult)
+  val transposedData = transpose(input)
+  // transposedData.map(calcDiagnosticData).pipe(reduceDiagnosticData).pipe(calcDiagnosticResult)
+  transposedData.map(calcDiagnosticData).pipe(data => locateLifeSupport(input, data)).pipe(calcDiagnosticResult)
 
 def calcDiagnosticResult(data: DiagnosticData): Int =
   Integer.parseInt(data.gamma, 2) * Integer.parseInt(data.epsilon, 2)
@@ -16,10 +19,23 @@ def calcDiagnosticResult(data: DiagnosticData): Int =
 def reduceDiagnosticData(data: List[DiagnosticData]): DiagnosticData =
   data.reduce((a, b) => DiagnosticData(a.gamma + b.gamma, a.epsilon + b.epsilon))
 
+def locateLifeSupport(input: List[String], data: List[DiagnosticData]): DiagnosticData =
+  DiagnosticData(locate(input, data, d => d.gamma), locate(input, data, d => d.epsilon))
+
+def locate(input: List[String], data: List[DiagnosticData], locator: DiagnosticData => String): String =
+  @tailrec
+  def filterInput(filteredInput: List[String], index: Int = 0): String = filteredInput match
+    case Nil      => ""
+    case x :: Nil => x
+    case _ =>
+      filterInput(filteredInput.filter(a => a.charAt(index) == locator(data(index)).charAt(index)), index + 1)
+
+  filterInput(input)
+
 def calcDiagnosticData(data: String): DiagnosticData =
   val oneCount = data.count(_ == '1')
   val zeroCount = data.length - oneCount
-  if oneCount > zeroCount then DiagnosticData("1", "0") else DiagnosticData("0", "1")
+  if oneCount >= zeroCount then DiagnosticData("1", "0") else DiagnosticData("0", "1")
 
 def transpose(input: List[String]): List[String] =
   def go(a: List[List[Char]]): List[List[Char]] = a.filter(_.nonEmpty) match
